bb4353ce369bc3999fee39a6cc828df35df3ac23
HOLMESsamuel <sam.holmes@live.fr>
2020-02-12 16:55:25 +0100

    Update webscraping.py
    modifying the program to get the available free trains on sncf website

--- webscraping.py
+++ webscraping.py
@@ -7,13 +7,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 import time
 
 
 browser = webdriver.Chrome('chromedriver.exe')
 
-url = "https://www.amazon.fr/"
+url = "https://www.oui.sncf/"
 header = {'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"} 
 
 
@@ -25,12 +26,22 @@ def getHTML(url, header):
 def openURL(url):
     browser.get(url)
 
-openURL(url)
 
-def search(keyword, browser):
-    search = browser.find_element_by_id('twotabsearchtextbox')
-    search.send_keys(keyword)
+
+def search(origin, destination,age, browser):
+    search = browser.find_element_by_id('vsb-origin-train-launch')
+    search.send_keys(origin)
+    time.sleep(3)
     search.send_keys(Keys.ENTER)
+    search = browser.find_element_by_id('vsb-destination-train-launch')
+    search.send_keys(destination)
+    time.sleep(3)
+    search.send_keys(Keys.ENTER)
+    select = Select(browser.find_element_by_id('passenger_1_train-launch-typos-in-card-typology'))
+    select.select_by_visible_text('12-25 ans')
+    search = browser.find_element_by_id('passenger_1_train-launch-typos-in-card_age')
+    search.send_keys(age)
+    
 
 
 def getContent(browser, header, type, selector):
@@ -38,12 +49,9 @@ def getContent(browser, header, type, selector):
     mydivs = soup.findAll(type, {"class": selector})
     return(mydivs)
 
-search("the witcher", browser)
-prices = getContent(browser, header, "span", "a-price-whole")
-titles = getContent(browser, header, "span", "a-size-medium a-color-base a-text-normal")
 
-for price, title in zip(prices, titles):
-    print(price.text, title.text)
 
-browser.quit()
+openURL(url)
+search("Gare de l'Est (Paris) (Île-de-France)", "Metz Ville (Grand Est)","21", browser)
+
 
